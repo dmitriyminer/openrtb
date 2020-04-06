2c85af86b5f3daa16ab04ca0fd7ac959492e367c
Dmytro Tananayskiy <dmitriyminer@gmail.com>
2020-04-06 18:44:06 +0300

    Add ImageTypeID to native/response/image.Image

--- native/response/image.go
+++ native/response/image.go
@@ -2,8 +2,18 @@ package response
 
 import "encoding/json"
 
+type ImageTypeID int
+
+const (
+	ImageTypeIcon ImageTypeID = 1 // Icon image
+	ImageTypeLogo ImageTypeID = 2 // Logo image for the brand/app
+	ImageTypeMain ImageTypeID = 3 // Large image preview for the ad
+)
+
 // Image object contains response image.
 type Image struct {
+	TypeID ImageTypeID `json:"type,omitempty"` // Type ID of the image element supported by the publisher
+
 	URL    string          `json:"url,omitempty"` // URL of the image asset
 	Width  int             `json:"w,omitempty"`   // Width of the image in pixels
 	Height int             `json:"h,omitempty"`   // Height of the image in pixels
